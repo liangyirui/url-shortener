d43100927f7e62b10ec2e4c08cb0fcd2be3f9fb6
Yirui Liang <yirui.liang@arb.ca.gov>
2025-01-16 16:15:52 -0800

    Implements shorten functionalities

--- api/main.go
+++ api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
+	"github.com/liangyirui/url-shortener/routes"
 )
 
 func setupRoutes(app *fiber.App) {
